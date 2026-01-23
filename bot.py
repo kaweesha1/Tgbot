1a85780a740717188d8821952ab9d76bbaf0e87b
Kaveesha Vadanu <153086805+kaweesha1@users.noreply.github.com>
2026-01-23 15:03:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ.get("8518142175:AAHtMpTE4I6DtCOZNS4KvjtgoOaTtEUB-t8")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
